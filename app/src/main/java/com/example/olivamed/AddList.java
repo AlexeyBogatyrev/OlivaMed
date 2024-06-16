d2c7bd2401cb19561825ccfa18087d4cc3ea601c
Alexey Bogatyrev <alex.bog04@mail.ru>
2024-06-17 00:04:05 +0300

    1

--- app/src/main/java/com/example/olivamed/AddList.java
+++ app/src/main/java/com/example/olivamed/AddList.java
@@ -9,11 +9,32 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class AddList extends AppCompatActivity {
 
+    EditText namezap;
+    EditText telzap;
+    EditText emailzap;
+    EditText comentzap;
+    RadioButton rad1;
+
+    private OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +67,73 @@ public class AddList extends AppCompatActivity {
         startActivity(intent);
     }
     public void goToZapis(View v){
-        Toast.makeText(AddList.this, "В разработке!", Toast.LENGTH_SHORT).show();
+        zapic();
     }
 
+    private void zapic() {
+        rad1 = findViewById(R.id.radioButton2);
+        namezap = findViewById(R.id.name_zapic);
+        emailzap = findViewById(R.id.email_zapic);
+        telzap = findViewById(R.id.telephone_zapic);
+        comentzap = findViewById(R.id.coment_zapic);
+        String fi = namezap.getText().toString();
+        String email = emailzap.getText().toString();
+        String phone_number = telzap.getText().toString();
+        String comment = comentzap.getText().toString();
 
+        JSONObject zapicData = new JSONObject();
+        try {
+            zapicData.put("fi", fi);
+            zapicData.put("email", email);
+            zapicData.put("phone_number", phone_number);
+            zapicData.put("comment", comment);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json; charset=utf-8"),
+                zapicData.toString()
+        );
+
+        Request request = new Request.Builder()
+                .url("http://79.174.82.176:8000/api/callback/")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(AddList.this, "Запись провалена", Toast.LENGTH_SHORT).show();
+                        System.out.println(e.getMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful() & rad1.isChecked()) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(AddList.this, "Запись успешна", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(AddList.this, "Запись провалена", Toast.LENGTH_SHORT).show();
+                            System.out.println(response.toString());
+                        }
+                    });
+                }
+            }
+        });
+    }
 }
 
--- app/src/main/java/com/example/olivamed/MainActivity.java
+++ app/src/main/java/com/example/olivamed/MainActivity.java
@@ -12,8 +12,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
     Dialog dialog;
+    EditText emailtext;
+    EditText emailtext2;
+    EditText usnametext;
+    EditText passtext;
+    EditText passtext2;
+    private OkHttpClient client = new OkHttpClient();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         but_log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "В разработке!", Toast.LENGTH_SHORT).show();
+                auent();
             }
         });
 
@@ -60,16 +81,142 @@ public class MainActivity extends AppCompatActivity {
 
         dialog.show();
     }
+    private void register() {
+
+        emailtext = dialog.findViewById(R.id.login);
+        usnametext = dialog.findViewById(R.id.usname);
+        passtext = dialog.findViewById(R.id.pass);
+        String email = emailtext.getText().toString();
+        String username = usnametext.getText().toString();
+        String password = passtext.getText().toString();
+
+        JSONObject registrationData = new JSONObject();
+        try {
+            registrationData.put("username", username);
+            registrationData.put("email", email);
+            registrationData.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json; charset=utf-8"),
+                registrationData.toString()
+        );
+
+        Request request = new Request.Builder()
+                .url("http://79.174.82.176:8000/api/users/create/")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Регистрация провалена", Toast.LENGTH_SHORT).show();
+                        System.out.println(e.getMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Регистрация успешна", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Регистрация провалена", Toast.LENGTH_SHORT).show();
+                            System.out.println(response.toString());
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void auent() {
+        emailtext2 = dialog.findViewById(R.id.loginpol);
+        passtext2 = dialog.findViewById(R.id.password);
+        String emailpole = emailtext2.getText().toString();
+        String passwordpole = passtext2.getText().toString();
+
+        JSONObject loginData = new JSONObject();
+        try {
+            loginData.put("username", emailpole);
+            loginData.put("password", passwordpole);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json; charset=utf-8"),
+                loginData.toString()
+        );
+
+        Request request = new Request.Builder()
+                .url("http://79.174.82.176:8000/api/users/login/")
+                .post(body)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Данные неверные или не совпадают", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    // Handle successful login here, e.g., saving tokens, navigating to another activity, etc.
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Вход успешен", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity.this, Base.class);
+                                startActivity(intent);
+
+                        }
+                    });
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Данные неверные или не совпадают", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
     private void showCustomDialogRegistration() {
+
         dialog.setContentView(R.layout.dialog_registration);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.setCancelable(true);
         Button registration = dialog.findViewById(R.id.button_registration);
 
+
         registration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "В разработке!", Toast.LENGTH_SHORT).show();
+                register();
+
             }
         });
         dialog.show();
